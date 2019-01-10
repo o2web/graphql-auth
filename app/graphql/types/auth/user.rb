63940d8cdd0f4ddae65690ddc5e873cb5536da2d
Guillaume Ferland <ferland182@gmail.com>
2019-01-10 17:33:24 +0100

    add user id to user type

--- app/graphql/types/auth/user.rb
+++ app/graphql/types/auth/user.rb
@@ -3,6 +3,10 @@
 class Types::Auth::User < GraphQL::Schema::Object
   description 'Data of a user'
 
+  field :id, ID, null: false do
+    description 'ID of the user'
+  end
+
   field :email, String, null: false do
     description 'Email address of the user'
   end
