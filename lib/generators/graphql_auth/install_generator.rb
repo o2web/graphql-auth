cad46ee4c45ed5109b6574b605270343c83aa934
Brice Sanchez <bricesanchez@users.noreply.github.com>
2019-02-13 14:13:14 -0500

    Refactor graphql_auth initializer to use configurations

--- lib/generators/graphql_auth/install_generator.rb
+++ lib/generators/graphql_auth/install_generator.rb
@@ -4,7 +4,7 @@ module GraphqlAuth
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_configuration
-        template 'initializer.rb', 'config/initializers/graphql_auth.rb'
+        template 'graphql_auth.rb.erb', 'config/initializers/graphql_auth.rb'
         template 'add_refresh_token_to_user.rb', "db/migrate/#{Time.now.strftime('%Y%m%d%H%M%S')}_add_refresh_token_to_user.rb"
       end
     end
