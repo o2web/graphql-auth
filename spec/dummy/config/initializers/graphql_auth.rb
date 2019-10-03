ffd7a512f277dfb7b44be8567dc3c10460cc0a04
Brice Sanchez <bricesanchez@users.noreply.github.com>
2019-10-02 23:35:03 -0400

    Update dummy graphql_auth initializer

--- spec/dummy/config/initializers/graphql_auth.rb
+++ spec/dummy/config/initializers/graphql_auth.rb
@@ -4,14 +4,17 @@ GraphQL::Auth.configure do |config|
   # config.app_url = ENV['APP_URL']
 
   # config.user_type = '::Types::Auth::User'
+  # config.sign_up_input_type = '::Types::Auth::Inputs::SignUp'
+  # config.update_account_input_type = '::Types::Auth::Inputs::UpdateAccount'
+  # config.update_password_input_type = '::Types::Auth::Inputs::UpdatePassword'
 
   # Devise allowed actions
   # Don't forget to enable the lockable setting in your Devise user model if you plan on using the lock_account feature
-  config.allow_sign_up = true
+  # config.allow_sign_up = true
   config.allow_lock_account = true
   config.allow_unlock_account = true
 
   # Allow custom mutations for signup and update account
   # config.sign_up_mutation = '::Mutations::Auth::SignUp'
-  # config.udpate_account_mutation = '::Mutations::Auth::UpdateAccount'
+  # config.update_account_mutation = '::Mutations::Auth::UpdateAccount'
 end
