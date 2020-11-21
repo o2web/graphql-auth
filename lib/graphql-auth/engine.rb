b6589c94af4212c2e9e691cd8667d9b6850987ad
Brice Sanchez <bricesanchez@users.noreply.github.com>
2020-11-21 01:14:51 -0500

    Fix autoload_paths
    It allows to start the application with zeitwerk

--- lib/graphql-auth/engine.rb
+++ lib/graphql-auth/engine.rb
@@ -3,7 +3,7 @@ module GraphQL
     class Engine < ::Rails::Engine
       isolate_namespace GraphQL::Auth
 
-      config.autoload_paths += Dir["#{config.root}/app/**/"]
+      config.autoload_paths += Dir["#{config.root}/app/**/*.rb"]
     end
   end
 end
